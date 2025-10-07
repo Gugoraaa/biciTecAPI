39417ba9247fb72dc11af57b3095ca81976d5030
Gugoraaa <gus.gonzalez.ramos@gmail.com>
2025-10-06 21:14:14 -0600

    fix: cors error

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import express from "express";
-import StationRoutes from "./routes/stations";
+import cors from "cors";
+import StationRoutes from "./routes/stations.js";
+
 const app = express();
 
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",       
+    "http://localhost:3000",       
+    "https://bici-tec.vercel.app"      
+  ],
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  credentials: true,               
+};
+
+
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use("/stations", StationRoutes);
 
 app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+  console.log("Server is running on port 4000");
 });
-
